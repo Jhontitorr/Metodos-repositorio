7c87f64030ef34305b4aa567ad878d5696af5f8f
Jhontitorr <73254498+Jhontitorr@users.noreply.github.com>
2020-10-21 17:21:37 -0300

    Add files via upload

---
+++ main_loopcliente.c
@@ -0,0 +1,19 @@
+#include "cliente.h"
+#include <unistd.h>
+#include <stdint.h>
+
+int main()
+{
+  int clientSocket = inicializa_e_conecta(7891);
+  char buffer[1024];
+  int i = 0;
+  while (1)
+  {
+    sleep(3);
+    sprintf(buffer,"Quem quer cafe? %d\n", i);
+    send(clientSocket,buffer,strlen(buffer)+1,0);
+    printf("Data sent: %s", buffer);
+    i++;
+  }
+  return 0;
+}
---
+++ main_loopservidor.c
@@ -0,0 +1,14 @@
+#include "servidor.h"
+#include <stdint.h>
+
+int main()
+{
+  int s_sock = inicializa_e_espera(7891);
+  char buffer[1024];
+  while (1)
+  {
+    if (recv(s_sock, buffer, 1024, 0))
+      printf("Data received: %s",buffer);
+  }
+  return 0;
+}
